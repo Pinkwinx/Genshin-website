f0d0d3e7667b982f0e3857a67d325ea589f284dc
Maria Nguyen <pinkwinx999@gmail.com>
2023-12-18 01:22:54 -0700

    new stuff :D

--- HTML/GenshinAnalysis.js
+++ HTML/GenshinAnalysis.js
@@ -3,7 +3,15 @@ const container = document.querySelector('.horizontal');
 const imageContainer = document.querySelector('.image-container');
 const lumineIdle = document.querySelector('.Lumine-Idle');
 const lumineRun = document.querySelector('.Lumine-Run');
-
+//if phone is in portrain mode
+window.addEventListener('orientationchange', function() {
+    var portraitMessage = document.querySelector('.portrait-message');
+    if (window.orientation === 0 || window.orientation === 180) {
+      portraitMessage.style.display = 'block';
+    } else {
+      portraitMessage.style.display = 'none';
+    }
+  });
 //button
 document.getElementById("click").onclick = function() {
     const titleDiv = document.querySelector('.title');
@@ -160,13 +168,29 @@ let verison = ['1.0','1.1','1.2','1.3','1.4','1.5','1.6','2.0','2.1','2.2','2.3'
                 ],
                 backgroundColor: 'rgb(17, 22, 99)',
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Version Updates'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Revenue Generated (in millions)'
+                    }
+                }
+            }
         }
     })
 //list for characters and rgb
 let char = ['Albedo','Alhaitham','Arataki Itto','Baizhu','Cyno','Dehya','Eula','Ganyu','Hu Tao','Kaedehara Kazuha','Kamisato Ayaka','Kamisato Ayato','Keqing','Klee','Lyney','Nahida','Nilou','Raiden Shogun','Sangonomiya Kokomi','Shenhe','Tartaglia','Tighnari','Venti','Wanderer','Xiao','Yae Miko','Yelan','Yoimiya','Zhongli']
 let charMode = [3,2,3,1,2,1,3,4,3,3,3,2,1,4,1,2,2,3,2,2,5,1,4,2,4,3,3,4,5]
-let charRev = [44.57,35.82,53.12,9.11,32.25,13.20,36.10,67.24,84.26,52.08,88.35,60.69,9.51,61.02,11.47,60.12,43.75,104.50,16.15,52.96,55.67,19.07,89.07,36.26,80.68,42.70,90.21,64.20,81.37]
-let charAbyss = [28.44,39.15,18.32,49.07,16.33,9.85,16.03,40.13,51.59,79.83,48.88,26.86,8.14,19.82,34.30,78.87,41.76,67.83,59.76,32.94,33.67,15.61,40.69,17.58,22.89,33.07,75.28,22.61,75.52]
+let charRev = [44.57,35.82,53.12,9.11,32.25,13.20,36.10,67.24,84.26,52.08,88.35,60.69,9.51,61.02,11.47,60.152,43.75,104.50,16.15,52.96,55.67,19.07,89.07,36.26,80.68,42.70,90.151,64.20,81.37]
+let charAbyss = [28.44,39.15,18.32,49.07,16.33,9.85,16.03,40.153,51.59,79.83,48.88,26.86,8.14,19.82,34.30,78.87,41.76,67.83,59.76,32.94,33.67,15.61,40.69,17.58,22.89,33.07,75.28,22.61,75.52]
 let charbg = [
     'rgba(255, 205, 86)',
     'rgba(109, 170, 29)',
@@ -199,124 +223,128 @@ let charbg = [
     'rgba(255, 205, 86)'
   ]
   let tartagliabg = [
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(134, 167, 201, 0.2)',
+    'rgba(255, 205, 86, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(255, 205, 86, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(134, 167, 201, 0.15)',
     'rgba(54, 162, 235)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)'
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(255, 205, 86, 0.15)'
   ]
   let cynobg = [
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
+    'rgba(255, 205, 86, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(255, 205, 86, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
     'rgba(153, 102, 255)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)'
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(54, 162, 235, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(255, 205, 86, 0.15)'
   ]
   let kazuhabg = [
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 205, 86, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(255, 205, 86, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
     'rgba(75, 192, 192)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)'
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(54, 162, 235, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(255, 205, 86, 0.15)'
   ]
   let eulabg = [
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 205, 86, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(255, 205, 86, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
     'rgba(134, 167, 201)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(134, 167, 201, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(109, 170, 29, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(54, 162, 235,0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)'
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(134, 167, 201, 0.15)',
+    'rgba(54, 162, 235, 0.15)',
+    'rgba(109, 170, 29, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(75, 192, 192, 0.15)',
+    'rgba(153, 102, 255, 0.15)',
+    'rgba(54, 162, 235,0.15)',
+    'rgba(255, 159, 64, 0.15)',
+    'rgba(255, 205, 86, 0.15)'
   ]
   //making new bar graphs
     new Chart(document.getElementById("barGraph1"), {
@@ -330,6 +358,22 @@ let charbg = [
                   borderColor: charbg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'The amount of times a character has made a reapperance'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("barGraph2"), {
@@ -343,6 +387,22 @@ let charbg = [
                   borderColor: charbg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Total revenue a character has made (in millions)'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("tartagliaGraph1"), {
@@ -356,6 +416,22 @@ let charbg = [
                   borderColor: tartagliabg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'The amount of times a character has made a reapperance'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("tartagliaGraph2"), {
@@ -369,6 +445,22 @@ let charbg = [
                   borderColor: tartagliabg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Total revenue a character has made (in millions)'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("cynoGraph1"), {
@@ -382,6 +474,22 @@ let charbg = [
                   borderColor: cynobg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'The amount of times a character has made a reapperance'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("cynoGraph2"), {
@@ -395,6 +503,22 @@ let charbg = [
                   borderColor: cynobg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Total revenue a character has made (in millions)'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("barGraph3"), {
@@ -408,6 +532,22 @@ let charbg = [
                   borderColor: charbg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Avarage Spiral Abyss usuage rate of each character (in percentage)'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("kazuhaGraph1"), {
@@ -421,6 +561,22 @@ let charbg = [
                   borderColor: kazuhabg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Avarage Spiral Abyss usuage rate of each character (in percentage)'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("kazuhaGraph2"), {
@@ -434,6 +590,22 @@ let charbg = [
                   borderColor: kazuhabg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Total revenue a character has made (in millions)'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("eulaGraph1"), {
@@ -447,6 +619,22 @@ let charbg = [
                   borderColor: eulabg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Avarage Spiral Abyss usuage rate of each character (in percentage)'
+                    }
+                }
+            }
         }
     })
     new Chart(document.getElementById("eulaGraph2"), {
@@ -460,6 +648,22 @@ let charbg = [
                   borderColor: eulabg,
                   borderWidth: 1
             }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Character name'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Total revenue a character has made (in millions)'
+                    }
+                }
+            }
         }
     })
 //add things to html
--- README.md
+++
@@ -1,3 +0,0 @@
-# Genshin-website
-Hello there, I am currently working on making a website about Genshin Impact's revenue stats and how correlate with the in-game meta.
-Currently I'm just analyzing data to before I being to make the html
